857cff1297cb7deb5c6f1a9d1390f9ff655f87bf
Mako? Rehaps? <72350714+MakoRehaps@users.noreply.github.com>
2026-08-19 02:54:01 -0700

    Route F1 map event checks away from F2 area data

--- src/map.h
+++ src/map.h
@@ -146,6 +146,7 @@ int _map_save_in_game(bool a1);
 #include "unified_worldmap_vehicle_profile.h"
 #include "unified_fallout1_encounter_runtime.h"
 #include "unified_fallout1_encounter_bridge.h"
+#include "unified_fallout1_worldmap_events.h"
 #include "unified_worldmap_lifecycle_profile.h"
 
 #endif /* MAP_H */
