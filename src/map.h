6df6bb3ddc068422263f23abc185d5adc32d619c
Mako? Rehaps? <72350714+MakoRehaps@users.noreply.github.com>
2026-08-19 02:29:28 -0700

    Route F1 worldmap lifecycle through sidecar backend

--- src/map.h
+++ src/map.h
@@ -142,5 +142,6 @@ int _map_save_in_game(bool a1);
 #include "unified_worldmap_state_profile.h"
 #include "unified_worldmap_grid_profile.h"
 #include "unified_loaded_map_profile.h"
+#include "unified_worldmap_lifecycle_profile.h"
 
 #endif /* MAP_H */
