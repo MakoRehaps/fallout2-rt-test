6e90985a2203e3bf3c2067427c6fcd1004b0368f
Mako? Rehaps? <72350714+MakoRehaps@users.noreply.github.com>
2026-08-18 22:29:26 -0700

    Drive live shared inventory from co-op frame hook

--- src/main.h
+++ src/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "local_coop_inventory_ui.h"
 #include "local_coop_runtime.h"
 #include "unified_campaign.h"
 
@@ -15,6 +16,8 @@ int inputGetInput();
 inline int localCoopMainInputGetInput()
 {
     localCoopRuntimeTick();
+    localCoopInventoryUiEnsureTicker();
+    localCoopInventoryUiTick();
     return inputGetInput();
 }
 
