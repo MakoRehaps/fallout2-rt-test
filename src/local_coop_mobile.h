a46957083c76709a580a867eb15be1fdcf4f9eb7
Mako? Rehaps? <72350714+MakoRehaps@users.noreply.github.com>
2026-08-25 04:02:57 -0700

    Add mobile controller host interface

---
+++ src/local_coop_mobile.h
@@ -0,0 +1,11 @@
+#ifndef LOCAL_COOP_MOBILE_H
+#define LOCAL_COOP_MOBILE_H
+
+namespace fallout {
+
+void localCoopMobileTick();
+void localCoopMobileShutdown();
+
+} // namespace fallout
+
+#endif /* LOCAL_COOP_MOBILE_H */
